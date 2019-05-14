130960ecc9ffcb5ede3ae1674b48f8c9e5957e11
theperkinrex <theperkinrex@gmail.com>
2019-05-14 18:16:06 +0200

    Start Rust impl

--- programs/program.armes.asm
+++
@@ -1,9 +0,0 @@
-$0 1
-$1 1
-LDA $0
-LDB $1
-!adding ADD
-JO !halting
-OUTB
-JMP !adding
-!halting HLT
--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from utils.bus import Bus
-from utils.memory import Memory, ROM, loadRAM
-from utils.register import Register, ConnectedRegister, InstructionRegister
-from utils.output import Output
-from utils.ALU import ALU
-from utils.flag import OVERFLOW, UNDERFLOW, ZERO, FlagRegister
